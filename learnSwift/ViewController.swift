611056a4adb0a1834ea0ddfa35a3d232a46f794a
oahgnehzoul <oahgnehzoul@gmail.com>
2016-10-28 16:42:57 +0800

    initialize

---
+++ learnSwift/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  learnSwift
+//
+//  Created by oahgnehzoul on 2016/10/28.
+//  Copyright © 2016年 oahgnehzoul. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("learn swift!")
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
